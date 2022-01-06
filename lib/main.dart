73f7e5b3c95493af0d8fd7fb7f0b824e3d554585
FlutLab.io <>
2022-01-06 05:47:03 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal);
-
+      home: Scaffold(backgroundColor: Colors.teal),
+    ),
+  );
 }
