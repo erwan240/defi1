bc641cde1ff251a61db063aaa0307e4a3be27349
FlutLab.io <>
2022-01-06 09:30:42 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ void main() {
               ),
               Text(
                 "Bonjour la famille",
-                style: TextStyle(fontFamily: "SourceSansPro-Regular", fontSize: 28, color: Colors.teal.shade100),
+                style: TextStyle(fontFamily: "SourceSansPro-Regular", fontSize: 28, color: Colors.teal.shade100, fontWeight: FontWeight.bold),
               ),
             ],
           ),
