b645c6e770c7b4783573142edd5b0f1223020ac9
FlutLab.io <>
2022-01-06 09:16:24 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,17 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: Center(
-          child: CircleAvatar(
-            backgroundImage: NetworkImage('https://static.lpnt.fr/images/2017/07/03/9271209lpw-9271595-article-francepoliticsislamreligiondinnerramadan-jpg_4395055_660x281.jpg'),
-            radius: 100,
+          child: Column(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage('https://static.lpnt.fr/images/2017/07/03/9271209lpw-9271595-article-francepoliticsislamreligiondinnerramadan-jpg_4395055_660x281.jpg'),
+                radius: 100,
+              ),
+              Text(
+                "Bonjour la famille",
+                style: TextStyle(fontFamily: SourceSansPro - Regular),
+              ),
+            ],
           ),
         ),
       ),
