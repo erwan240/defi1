548935aebfef2a3f43db5b56dbeb422d8e486f3b
FlutLab.io <>
2022-01-06 05:45:25 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -5,45 +5,6 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  color: Colors.redAccent,
-                  height: 700,
-                  width: 100,
-                ),
-                Container(
-                  height: 730,
-                  width: 100,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 100,
-                        color: Colors.amber,
-                      ),
-                      Container(
-                        width: 100,
-                        height: 100,
-                        color: Colors.green,
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  width: 100,
-                  height: 730,
-                  color: Colors.lightBlueAccent,
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
+        body: SafeArea(),
+
 }
