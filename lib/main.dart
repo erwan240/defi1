fd686f2119c4d3be3707428d684f8d084e4377ba
FlutLab.io <>
2022-01-06 05:56:03 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.teal,
-            body: Center(
-                child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      'https://static.lpnt.fr/images/2017/07/03/9271209lpw-9271595-article-francepoliticsislamreligiondinnerramadan-jpg_4395055_660x281.jpg'),
-                  radius: 60,
-                ),
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Center(
+          child: CircleAvatar(
+            backgroundImage: NetworkImage('https://static.lpnt.fr/images/2017/07/03/9271209lpw-9271595-article-francepoliticsislamreligiondinnerramadan-jpg_4395055_660x281.jpg'),
+            radius: 60,
+          ),
+        ),
+      ),
+    ),
+  );
+}
