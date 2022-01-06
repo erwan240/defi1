61b93f63687b5846ab1b8a6ad0f22e8bed0fb8eb
FlutLab.io <>
2022-01-06 05:49:41 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold(backgroundColor: Colors.teal),
-    ),
-  );
-}
+        home: Scaffold(
+            backgroundColor: Colors.teal,
+            body: Center(
+                child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://images.unsplash.com/photo-1640112764053-0e0c7120e7f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=726&q=80'),
+                  radius: 60,
+                ),
