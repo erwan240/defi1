6b52dcd27a2f6d888c8c055c8149e14d5fe3316d
FlutLab.io <>
2022-01-10 05:05:31 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,12 @@ void main() {
                 backgroundImage: NetworkImage('https://static.lpnt.fr/images/2017/07/03/9271209lpw-9271595-article-francepoliticsislamreligiondinnerramadan-jpg_4395055_660x281.jpg'),
                 radius: 100,
               ),
-              Padding(padding: EdgeInsets.all(16.0)
+              Padding(
+                padding: EdgeInsets.all(16.0),
                 Text(
                   "Erwan Hubert",
                   style: TextStyle(fontFamily: "SourceSansPro-Regular", fontSize: 28, color: Colors.teal.shade100, fontWeight: FontWeight.bold, letterSpacing: 2.5),
-                ),
+                ), 
               ),
             ],
           ),
