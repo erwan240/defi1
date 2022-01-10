6b205ecc27e4da2eafb8988963fbdbbb4a77f163
FlutLab.io <>
2022-01-10 06:54:17 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,18 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundImage: NetworkImage("https://images.bfmtv.com/e2I_ZxiGrfuDUU1BDOOq0dMY44M=/3x5:4947x2786/640x0/images/-128791.jpg"),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     ),
   );
