bcd83d0ffe9dfec13710c07abdfbea77d4c567c2
FlutLab.io <>
2022-01-10 04:59:23 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,11 @@ void main() {
                 backgroundImage: NetworkImage('https://static.lpnt.fr/images/2017/07/03/9271209lpw-9271595-article-francepoliticsislamreligiondinnerramadan-jpg_4395055_660x281.jpg'),
                 radius: 100,
               ),
-              Text(
-                "Bonjour la famille",
-                style: TextStyle(fontFamily: "SourceSansPro-Regular", fontSize: 28, color: Colors.teal.shade100, fontWeight: FontWeight.bold, letterSpacing: 2.5),
+              Padding(padding: EdgeInsets.all(16.0)
+                Text(
+                  "Erwan Hubert",
+                  style: TextStyle(fontFamily: "SourceSansPro-Regular", fontSize: 28, color: Colors.teal.shade100, fontWeight: FontWeight.bold, letterSpacing: 2.5),
+                ),
               ),
             ],
           ),
